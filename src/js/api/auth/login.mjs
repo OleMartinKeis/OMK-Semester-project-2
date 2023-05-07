f4ec7064c4382466001e7d3ccb9c4ee0e6cd8f27
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-05-07 09:59:41 +0200

    added login and register function

---
+++ src/js/api/auth/login.mjs
@@ -0,0 +1,36 @@
+import { API_AUCT_URL } from "../constants.mjs";
+import * as storage from "../storage/index.mjs";
+
+
+const path ="/auth/login";
+const method ="POST";
+/**
+ * This function checks your login credentials with the server.
+ * If correct this will give you the accessToken (JWT) and let you for example; get a post feed and edit your profile!
+ * @param {object} profile list of necessities to access profile
+ */
+
+
+export async function login(profile) {
+
+  const loginURL = API_AUCT_URL + path;
+  const body = JSON.stringify(profile);
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body,
+  });
+
+
+  const { accessToken, ...user } = await response.json();
+
+  storage.save("token", accessToken);
+
+  storage.save("profile", user);
+
+  alert("You are logged in now");
+
+  location.href ="/posts/index.html"
+}
---
+++ src/js/api/auth/register.mjs
@@ -0,0 +1,37 @@
+import { API_AUCT_URL } from "../constants.mjs"
+
+const path ="/auth/register";
+const method ="POST";
+
+/**This will send a package to the server and check if you credentials isnt taken or invalid and create a user for you
+ *  If correct this will give you a accessToken (JWT) and let you for example; create a post and edit your profile!
+ * @param {object} profile  list of necessities to access profile
+ * @returns 
+ */
+
+
+export async function register(profile) {
+    const registerURL = API_AUCT_URL + path;
+    
+    try {
+        const response = await fetch(registerURL, {
+        
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method,
+        body: JSON.stringify(profile)
+        
+    });
+    const result = await response.json();
+    alert("You are now registered")
+    return result
+    }
+
+    catch{
+        console.log(error)
+    }
+register(profile)
+
+
+}  
