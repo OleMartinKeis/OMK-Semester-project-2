2d1976549a6808f8047463640db26d5d287b860b
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-05-08 09:34:44 +0200

    added create listing and barrel file

---
+++ src/js/api/listings/create.mjs
@@ -0,0 +1,39 @@
+import { API_AUCT_URL } from "../constants.mjs";
+
+import { fetchWithToken, headers } from "../fetchWithToken.mjs"
+
+const path ="/listings";
+const method ="POST";
+
+/** This fucntion will send a POST request to the server 
+ * setCreatePostListener()  will use this function to create the post
+ * 
+ * @param {string} listingData  data contents of post
+ * @returns 
+ */
+
+
+export async function createListing(listingData) {
+    const createListingURL = API_AUCT_URL + path;
+
+    const comma = ","
+
+    const tagsArray = listingData.tags.split(comma);
+
+    const response = await fetchWithToken(createListingURL, {
+        method,
+        headers: headers("application/json"),
+        body: JSON.stringify({
+            title: listingData.title,
+            description: listingData.body,
+            endsAt: listingData.endsAt,
+            media: listingData.media,
+            tags: tagsArray,
+        }),
+    });
+    
+    // const listing = await response.json
+    // return listing
+
+    return await response.json();
+}
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,8 @@
 import { setLoginFormListener } from "./handlers/login.mjs";
 import { setRegisterFormListener } from "./handlers/register.mjs";
+import { createListing } from "./api/listings/create.mjs";
+import { getListings } from "./api/listings/read.mjs";
+
 
 const path = location.pathname;
 
@@ -7,6 +10,7 @@ if(path === '/index.html') {
     setLoginFormListener();
 } else if (path === '/profile/register') {
     setRegisterFormListener();
+} else if (path === '/listing/index.html') {
+    getListings().then(console.log)
 }
-
 
