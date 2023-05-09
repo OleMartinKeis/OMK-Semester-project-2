eb2d0f1a35b484d1212accd4b7dedfb4072dc358
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-05-09 15:13:54 +0200

    minor adjustments to main js file

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,16 +1,14 @@
-import { setLoginFormListener } from "./handlers/login.mjs";
-import { setRegisterFormListener } from "./handlers/register.mjs";
-import { createListing } from "./api/listings/create.mjs";
-import { getListings } from "./api/listings/read.mjs";
+import * as handlers from "./handlers/index.mjs"
 
 
+const container = document.querySelector("#listings");
 const path = location.pathname;
 
 if(path === '/index.html') {
-    setLoginFormListener();
+    handlers.setLoginFormListener();
 } else if (path === '/profile/register') {
-    setRegisterFormListener();
+    handlers.setRegisterFormListener();
 } else if (path === '/listing/index.html') {
-    getListings().then(console.log)
+    handlers.displayListings(container)
 }
 
