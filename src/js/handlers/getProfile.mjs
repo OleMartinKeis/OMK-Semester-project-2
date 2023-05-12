85bc8c089436d36f655c781ef9536bad62212217
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2023-05-12 08:14:48 +0200

    started on profile handlers

---
+++ src/js/handlers/getProfile.mjs
@@ -0,0 +1,27 @@
+import * as profileTemplate from "../templates/profile.mjs";
+import * as profileMethods from "../api/profile/index.mjs";
+import { load } from "../api/storage/index.mjs"
+
+const url = new URL(location.href);
+const name = url.searchParams.get("name");
+
+export async function readProfile() {
+
+    try{
+
+        const profile = load("profile")
+        if (!profile) {
+            location.href = "/"
+        }
+        const response = await profileMethods.getProfile(profile.name);
+        
+        const profileContainer = document.querySelector("#profileContainer");
+        
+        profileTemplate.renderProfileTemplate(response, profileContainer);
+        
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+} 
